cab158a571e176dd9e9b06921b67edc2bc42a7d2
Booster <wellingtonkr.pessoal@gmail.com>
2026-04-20 18:49:24 +0000

    archives

--- script.js
+++ script.js
@@ -1,43 +1,77 @@
 const container = document.getElementById("container");
 const searchInput = document.getElementById("search");
 
+function normalize(str) {
+  return str
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+}
+
+function escapeRegex(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
+
 fetch("https://raw.githubusercontent.com/RedeCanary/redecanary-requests/main/skyblock/items.json")
   .then(res => res.json())
   .then(data => {
 
     const items = Object.values(data);
 
-    function render(list) {
-      container.innerHTML = "";
+    // render
+    function render(list, value = "") {
+        container.innerHTML = "";
 
-      list.forEach(item => {
-        const card = document.createElement("div");
-        card.className = "card " + (item.rarity || "").toLowerCase();
+        list.forEach(item => {
+            const card = document.createElement("div");
+            card.className = "card " + (item.rarity || "").toLowerCase();
 
-        card.innerHTML = `
-          <h3>${item.displayName}</h3>
-          <div class="stats">
-            <p>${item.category}</p>
-            <p>💰 ${item.sellPrice}</p>
-            <p>❤ ${item.baseAttributes?.MAX_HEALTH || 0} | 🛡 ${item.baseAttributes?.DEFENSE || 0}</p>
-          </div>
-        `;
+            const name = (item.displayName || "");
 
-        container.appendChild(card);
-      });
+            const highlightedName = value
+                ? name.replace(
+                    new RegExp(escapeRegex(value), "gi"),
+                    match => `<span class="highlight">${match}</span>`
+                ) : name;
+
+            card.innerHTML = `
+            <h3>${highlightedName}</h3>
+            <div class="stats">
+                <p>${item.category}</p>
+                <p>💰 ${item.sellPrice}</p>
+                <p>❤ ${item.baseAttributes?.MAX_HEALTH || 0} | 🛡 ${item.baseAttributes?.DEFENSE || 0}</p>
+            </div>
+            `;
+
+            container.appendChild(card);
+        });
     }
+    // render
 
-    render(items);
+    render(items, "");
     console.log("itens carregados:", items.length);
 
+    let timeout;
+
     searchInput.addEventListener("keyup", () => {
-        const value = searchInput.value.toLowerCase();
+        clearTimeout(timeout);
 
-        const filtered = items.filter(item =>
-            (item.displayName || "").toLowerCase().includes(value)
-        );
+        timeout = setTimeout(() => {
+            const value = normalize(searchInput.value);
 
-        render(filtered);
+            const filtered = items.filter(item => {
+                const text = (
+                    (item.displayName || "") + " " +
+                    (item.category || "") + " " +
+                    (item.rarity || "")
+                ).toLowerCase();
+
+                return text.includes(value);
+            });
+
+            render(filtered, value);     
+        }, 200)
+        
     });
 
   });
