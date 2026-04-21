96ee095177fc84c97cc756a8b30df81705c4ef98
Booster <wellingtonkr.pessoal@gmail.com>
2026-04-21 22:25:49 +0000

    archives

--- src/App.js
+++ src/App.js
@@ -17,21 +17,19 @@ import SearchPage from "./pages/SearchPage";
 export default function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/items" element={<ItemsPage />} />
-          <Route path="/enchantments" element={<EncantamentosPage />} />
-          <Route path="/reforges" element={<ReforjasPage />} />
-          <Route path="/collections" element={<ColecoesPage />} />
-          <Route path="/pets" element={<PetsPage />} />
-          <Route path="/entities" element={<EntidadesPage />} />
-          <Route path="/getting-started" element={<GuiaPage />} />
-          <Route path="/rules" element={<RegrasPage />} />
-          <Route path="/tier-lists" element={<TierListsPage />} />
-          <Route path="/search" element={<SearchPage />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/items" element={<ItemsPage />} />
+        <Route path="/enchantments" element={<EncantamentosPage />} />
+        <Route path="/reforges" element={<ReforjasPage />} />
+        <Route path="/collections" element={<ColecoesPage />} />
+        <Route path="/pets" element={<PetsPage />} />
+        <Route path="/entities" element={<EntidadesPage />} />
+        <Route path="/getting-started" element={<GuiaPage />} />
+        <Route path="/rules" element={<RegrasPage />} />
+        <Route path="/tier-lists" element={<TierListsPage />} />
+        <Route path="/search" element={<SearchPage />} />
+      </Routes>
     </div>
   );
 }
